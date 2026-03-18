fe4d19cd3f088d91aee9d75ab955829030991f4a
Xiang xin wang <wxinxiang8@gmail.com>
2026-03-18 13:08:37 +0800

    1.0

---
+++ Core/Src/emm_motor.c
@@ -0,0 +1,9 @@
+/*
+ * @Author: Xiang xin wang wxinxiang8@gmail.com
+ * @Date: 2026-03-18 12:57:18
+ * @LastEditors: Xiang xin wang wxinxiang8@gmail.com
+ * @LastEditTime: 2026-03-18 12:59:53
+ * @FilePath: \MDK-ARMd:\shrimp_machine\shrimp_machine\Core\Src\emm_motor.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include "emm_motor.h"
---
+++ Core/Src/rm3508.c
@@ -0,0 +1,9 @@
+/*
+ * @Author: Xiang xin wang wxinxiang8@gmail.com
+ * @Date: 2026-03-18 12:56:58
+ * @LastEditors: Xiang xin wang wxinxiang8@gmail.com
+ * @LastEditTime: 2026-03-18 13:05:23
+ * @FilePath: \MDK-ARMd:\shrimp_machine\shrimp_machine\Core\Src\rm3508.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include "rm3508.h"
